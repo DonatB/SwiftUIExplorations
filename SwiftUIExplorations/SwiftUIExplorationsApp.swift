2b5738c567eb51a23577e06ae8e87121cad361d9
DonatB <donatbajrami@hotmail.com>
2025-04-01 19:24:54 +0200

    Initial Commit

---
+++ SwiftUIExplorations/SwiftUIExplorationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExplorationsApp.swift
+//  SwiftUIExplorations
+//
+//  Created by Donat on 1.4.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExplorationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
